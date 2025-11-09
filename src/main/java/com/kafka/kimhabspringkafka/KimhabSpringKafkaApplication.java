687f8daa918b586f2200e4d1392621fe4152905d
kimhab11 <keomhab@gmail.com>
2025-11-09 14:17:40 +0700

    init

---
+++ src/main/java/com/kafka/kimhabspringkafka/KimhabSpringKafkaApplication.java
@@ -0,0 +1,11 @@
+package com.kafka.kimhabspringkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimhabSpringKafkaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KimhabSpringKafkaApplication.class, args);
+    }
+}
